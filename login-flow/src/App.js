cdef1660a5359a3e27f343ea2e4f6accfe5bc1ef
winnetC <winnet.chibisa@gmail.com>
2025-12-01 12:02:12 +0200

    Set up routing in App.js

--- login-flow/src/App.js
+++ login-flow/src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Login from './components/Login';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route path="/login" component={Login} />
+        <Route path="/dashboard">
+          <h2>Welcome to the Dashboard!</h2>
+        </Route>
+      </Switch>
+    </Router>
   );
-}
+};
 
 export default App;
